0540c0e36ebfd53e33921f15a5537a8c4b28c42b
Patrick Mueller <pmuellr@apache.org>
2012-01-19 07:44:57 -0500

    added from local

---
+++ github-api.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ curl https://api.github.com/repos/pmuellr/wiki-test/git/refs/heads/master
+ curl https://api.github.com/repos/pmuellr/wiki-test/git/trees/2756f3022e07f9ebd28dbd71181d2456f7695ffc
+ curl https://api.github.com/repos/pmuellr/wiki-test/git/blobs/71d092683bc408b36c9199a9e1c2e1127083b8fc
